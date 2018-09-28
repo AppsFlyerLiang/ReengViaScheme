e8cae5cbf676eebf1b7295152befed9f3b5bb6a5
Liang Hu <liahoo@gmail.com>
2018-09-28 18:26:40 +0900

    Another way to ensure Re-engagement conversion.

--- app/src/main/java/com/appsflyer/support/reengviascheme/SchemeActivity.java
+++ app/src/main/java/com/appsflyer/support/reengviascheme/SchemeActivity.java
@@ -6,36 +6,67 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.appsflyer.AppsFlyerConversionListener;
 import com.appsflyer.AppsFlyerLib;
 
-public class SchemeActivity extends AppCompatActivity {
+import java.util.Map;
+
+//https://reengviasheme.onelink.me/gsho?pid=aftest&af_dp=reengviascheme%3A%2F%2Fred&is_retargeting=true
+
+public class SchemeActivity extends AppCompatActivity implements AppsFlyerConversionListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(AppsFlyerLib.LOG_TAG, "[SchemeActivity.onCreate]");
         setContentView(R.layout.activity_scheme);
         AppsFlyerLib.getInstance().sendDeepLinkData(this);
-        gotoCampaignByIntent(getIntent());
+        AppsFlyerLib.getInstance().registerConversionListener(this, this);
     }
 
-    private void gotoCampaignByIntent(Intent intent) {
-        Uri data = intent.getData();
-        if(data!=null && data.getScheme().equals("reengviascheme")){
-            String host = data.getHost();
-            if(host.equals("red")){
-                Intent newIntent=new Intent(this, CampaignRedActivity.class);
-                newIntent.setData(intent.getData());  // Work around
-                newIntent.setAction(intent.getAction()); // Work around
-                startActivity(newIntent);
-            } else if(host.equals("blue")) {
-                Intent newIntent=new Intent(this, CampaignBlueActivity.class);
-                newIntent.setData(intent.getData());  // Work around
-                newIntent.setAction(intent.getAction()); // Work around
-                startActivity(newIntent);
-            } else {
-                Log.w(AppsFlyerLib.LOG_TAG,"[gotoCampaignByIntent] host: [" + host +"]is not handled");
-            }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        AppsFlyerLib.getInstance().unregisterConversionListener();
+    }
+
+    @Override
+    public void onInstallConversionDataLoaded(Map<String, String> map) {
+
+    }
+
+    @Override
+    public void onInstallConversionFailure(String s) {
+
+    }
+
+    @Override
+    public void onAppOpenAttribution(Map<String, String> map) {
+        Log.d(AppsFlyerLib.LOG_TAG, "[onAppOpenAttribution]");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            Log.d(AppsFlyerLib.LOG_TAG, entry.getKey() + "=" + entry.getValue());
         }
+
+        String scheme=map.get("scheme");
+        String host=map.get("host");
+        String path=map.get("path");
+        if(host.equals("red")){
+            Intent newIntent=new Intent(this, CampaignRedActivity.class);
+//            newIntent.setData(getIntent().getData());
+            startActivity(newIntent);
+            finish();
+        } else if(host.equals("blue")) {
+            Intent newIntent=new Intent(this, CampaignBlueActivity.class);
+//            newIntent.setData(getIntent().getData());
+            startActivity(newIntent);
+            finish();
+        } else {
+            Log.w(AppsFlyerLib.LOG_TAG,"[gotoCampaignByIntent] host: [" + host +"]is not handled");
+        }
+    }
+
+    @Override
+    public void onAttributionFailure(String s) {
+
     }
 
 }
